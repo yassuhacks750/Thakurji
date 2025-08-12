dba217b4ea54bbc8dfd806638201fae512ff2c78
yassuhacks750 <yassudj8085231272@gmail.com>
2025-08-12 18:03:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,11 @@ class Config(object):
     DB_NAME = "Txt"
     API_ID = "22849789"
     API_HASH = "0fc127c6055acd59f00ec6c229e1e3c4"
-    ADMIN_ID = 
+    ADMIN_ID = "7296271316"
     DB_URL = "mongodb+srv://yassuthakur750:Yash2835P@txt.qz9t8qs.mongodb.net/?retryWrites=true&w=majority&appName=Txt"
     LOG_CHANNEL =  # Your Log Channel ID (Bot ko ADMIN BNAYE)
     USERLINK = ""
     TUTORIAL_VIDEO = ""
+
 
 
